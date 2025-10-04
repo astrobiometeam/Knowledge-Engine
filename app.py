fe9d4dc297176a5db49541f5992ad7c099e8dcc5
Astro-Biome <astrobiomteam@gmail.com>
2025-10-04 19:33:26 +0330

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,12 @@ import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
 
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 
 nlp.add_pipe("spacytextblob")
